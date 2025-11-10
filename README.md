8c81400d9635d9a71e39632cd3375864fa3fb42b
carlos <ccarbonell@gmail.com>
2025-11-10 14:12:29 +0100

    updated

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # twitchplay
 plays last video on twitch channel
+
+# Install
+run 
+```
+sudo bash install.sh
+```
--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
-pip install yt-dlp requests
+sudo add-apt-repository ppa:tomtomtom/yt-dlp    # Add ppa repo to apt
+sudo apt update                                 # Update package list
+sudo apt install yt-dlp    
+    sudo apt install chromium-driver
+pip install -r requirements.txt
